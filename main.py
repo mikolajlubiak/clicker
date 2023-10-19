43656a49e57bb0d7be9d3bd41db727cf16ac8641
Mikołaj Lubiak <lubiak@proton.me>
2023-10-19 22:04:24 +0200

    Use low level Windows API

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from pynput.mouse import Controller, Button
-from pynput.keyboard import Listener, KeyCode
+import ctypes
+import time
 import psutil
 
-app_name = 'metin2.exe'
+app_name = 'app.exe'
 
-mouse = Controller()
+VK_1 = 0x31
+VK_2 = 0x32
 
-keys = ((KeyCode.from_char('1'), (115, 297), Button.left), (KeyCode.from_char('2'), (1183, 123), Button.right))
+MOUSE_LEFTDOWN = 0x0002
+MOUSE_LEFTUP = 0x0004
+MOUSE_RIGHTDOWN = 0x0008
+MOUSE_RIGHTUP = 0x0010
+
+user32 = ctypes.windll.user32
 
 def is_app_running(app_name):
     for process in psutil.process_iter(attrs=['name']):
@@ -14,12 +20,28 @@ def is_app_running(app_name):
             return True
     return False
 
-def on_key_press(key):
-    for i in range(len(keys)):
-        if key == keys[i][0]:
-            if is_app_running(app_name):
-                mouse.position = keys[i][1]
-                mouse.click(keys[i][2], 1)
+def send_mouse_input(x, y, mouse_event):
+    extra = ctypes.c_ulong(0)
+    ii_ = ctypes.c_ulong(0)
 
-with Listener(on_press=on_key_press) as listener:
-    listener.join()
+    struct_input = ctypes.Structure(
+        [("type", ctypes.c_ulong), ("ii", ctypes.c_ulong)]
+
+    )
+    x = int(x * (65536 / ctypes.windll.user32.GetSystemMetrics(0)) + 1)
+    y = int(y * (65536 / ctypes.windll.user32.GetSystemMetrics(1)) + 1)
+
+    struct_input.type = 0x0000
+    struct_input.ii = (
+        (x & 0xFFFF) | ((y & 0xFFFF) << 16) | mouse_event
+    )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(struct_input), ctypes.sizeof(struct_input))
+
+while True:
+    if is_app_running(app_name):
+        if ctypes.windll.user32.GetKeyState(VK_1) < 0:
+            send_mouse_input(115, 297, MOUSE_LEFTDOWN | MOUSE_LEFTUP)
+            time.sleep(0.1)
+        elif ctypes.windll.user32.GetKeyState(VK_2) < 0:
+            send_mouse_input(1183, 123, MOUSE_RIGHTDOWN | MOUSE_RIGHTUP)
+            time.sleep(0.1)
